304dc3d80574b4c8af69fb78e474b093e9bfdf5b
njmarko <marko.njegomir@uns.ac.rs>
2022-05-29 15:44:00 +0200

    Made atributes work for CEP events.

--- neuralink-app/src/main/java/sbnz/mrsandman/neuralinkapp/model/events/SleepPhaseEvent.java
+++ neuralink-app/src/main/java/sbnz/mrsandman/neuralinkapp/model/events/SleepPhaseEvent.java
@@ -1,13 +1,49 @@
 package sbnz.mrsandman.neuralinkapp.model.events;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import org.kie.api.definition.type.Expires;
 import org.kie.api.definition.type.Role;
+import org.kie.api.definition.type.Timestamp;
+
+import sbnz.mrsandman.neuralinkapp.model.enums.SleepPhase;
 
 @Role(Role.Type.EVENT)
+//@Timestamp("executionTime")
 @Expires("30m")
 public class SleepPhaseEvent implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+//    private Date executionTime;
+	private SleepPhase sleepPhase;
 
+	public SleepPhaseEvent() {
+		super();
+	}
+	
+	public SleepPhaseEvent(SleepPhase sleepPhase) {
+		super();
+		this.sleepPhase = sleepPhase;
+	}
+
+	public SleepPhase getSleepPhase() {
+		return sleepPhase;
+	}
+
+	public void setSleepPhase(SleepPhase sleepPhase) {
+		this.sleepPhase = sleepPhase;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "SleepPhaseEvent [sleepPhase=" + sleepPhase + "]";
+	}
+
+	
+	
 }
