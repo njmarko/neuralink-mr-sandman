17ffc9874d63af1d2f94bc7ba5bb7c5259be7523
njmarko <marko.njegomir@uns.ac.rs>
2022-06-05 21:38:31 +0200

    Added rule for detecting raised physical event 2 hours before optimal sleep time.

--- neuralink-app/src/main/java/sbnz/mrsandman/neuralinkapp/model/events/physicalactivity/PhysicalActivityEvent.java
+++ neuralink-app/src/main/java/sbnz/mrsandman/neuralinkapp/model/events/physicalactivity/PhysicalActivityEvent.java
@@ -6,7 +6,7 @@ import org.kie.api.definition.type.Expires;
 import org.kie.api.definition.type.Role;
 
 @Role(Role.Type.EVENT)
-@Expires("30m")
+@Expires("12h")
 public class PhysicalActivityEvent implements Serializable {
 
 	private static final long serialVersionUID = 1L;
